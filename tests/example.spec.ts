dc6a740a988133951488075884a6582c6956a604
Malith212 <malithnavindu130@gmail.com>
2026-04-11 15:39:52 +0530

    test: implement login test with email and password fields

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Login', async ({ page }) => {
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+    //locators
+    const email = page.locator("//input[@type='email']");
+    const password = page.locator("//input[@type='password']");
+    const loginBtn = page.locator("//input[@type='submit']");
+ 
+    //values
+    const emailValue = "navindumalith0@gmail.com";
+    const passwordValue = "Malith123@";
+ 
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+    await page.goto('https://rahulshettyacademy.com/client');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+    await email.fill(emailValue);
+    await password.fill(passwordValue);
+    await loginBtn.click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+
+})
